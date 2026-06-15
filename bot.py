90e95e6008b62008d5ed11fa0597a0c1aea2cd0e
davidherborn-cmyk <david_herborn@web.de>
2026-06-15 15:42:47 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,11 @@ from flask import Flask
 from threading import Thread
 import os
 
-# --- Webserver-Trick für Render ---
 app = Flask('')
+
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "Bot is active"
 
 def run_web():
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
@@ -18,18 +18,13 @@ def keep_alive():
     t = Thread(target=run_web)
     t.start()
 
-# --- Dein eigentlicher Bot ---
-TOKEN = "8998358080:Here is the token for bot GoldWaechter_123_bot @GoldWaechter_David_bot:
-
-8998358080:AAF-NpC7oFHAQvlNwhAEpnXfr1UNFnb4V5M
+TOKEN = "8998358080:AAF-Npc7oFHAqv1NwhAepnXfr1UNFnb4V5M"
 
 async def main():
     application = ApplicationBuilder().token(TOKEN).build()
-    print("Bot läuft erfolgreich in der Cloud...")
+    print("Bot is running...")
     await application.run_polling()
 
 if __name__ == '__main__':
-    keep_alive() # Startet den Webserver-Trick
-    asyncio.run(main()) # Startet den Bot
-    
-
+    keep_alive()
+    asyncio.run(main())
